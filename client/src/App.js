5d7f3a2e09ed16a888057c219110f723d3db32bc
aadhith <aadhith2000@gmail.com>
2020-09-29 00:44:30 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,11 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.css"
 import Form from "react-bootstrap/Form"
 import Col from "react-bootstrap/Col"
+import Button from "react-bootstrap/Button"
+import Navbar from "react-bootstrap/Navbar"
+import Jumbotron from "react-bootstrap/Jumbotron"
+import ProgressBar from "react-bootstrap/ProgressBar"
+import Row from "react-bootstrap/Row"
 
 class App extends Component {
   state = { web3: null, accounts: null, contract: null, noOfTokens: 0, tokensSold: 1, tokenPrice: 0, userTokens: null };
@@ -31,14 +36,14 @@ class App extends Component {
         CrossbarToken.networks[networkId] && CrossbarToken.networks[networkId].address
       );
 
-      const _tokensSold = await this.tokenSaleInstance.methods.tokensSold().call();
       const _tokenPrice = await this.tokenSaleInstance.methods.price().call();
+      const _tokensSold = await this.tokenSaleInstance.methods.tokensSold().call();
       const _userTokens = await this.tokenInstance.methods.balanceOf(accounts[0]).call();
 
       this.handleSoldEvent();
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: this.tokenSaleInstance, tokensSold: _tokensSold, tokenPrice: _tokenPrice, userTokens: _userTokens });
+      this.setState({ web3, accounts, contract: this.tokenSaleInstance, tokensSold: _tokensSold, userTokens: _userTokens, tokenPrice: _tokenPrice });
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -57,6 +62,7 @@ class App extends Component {
 
   handleSubmit = async (event) => {
     event.preventDefault();
+    console.log(typeof this.state.noOfTokens)
     if (this.state.noOfTokens <= 0) {
       alert("No of tokens bought should be greater than 0");
     }
@@ -83,26 +89,62 @@ class App extends Component {
 
   render() {
     if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
+      return <div>
+        Loading Web3, accounts, and contract...</div>;
     }
     return (
       <div className="App">
-        <h1>Crossbar Token Sale</h1>
-        <p>Get your tokens here.</p>
-        <p>Tokens Sold: {this.state.tokensSold}</p>
-        <p>Token Price: {this.state.tokenPrice}</p>
-        <p>Your Tokens: {this.state.userTokens}</p>
-        <Form>
-          <Form.Row>
-            <Col xs={2} justify="center" n>
-              <Form.Control placeholder="First name" onChange={this.handleChange} />
-            </Col>
-          </Form.Row>
-        </Form>
-        <form onSubmit={this.handleSubmit}>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="/">
+            Crossbar Token Crowdsale
+          </Navbar.Brand>
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+              Your Account: {this.state.accounts[0]}
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Navbar>
+
+        <Jumbotron>
+          <h3>Introducing Crossbar Token (CBR). Get yours here!</h3>
+          <div class="container">
+            <Row>
+              <Col >
+                <p>Token Price: 0.01 ETH</p>
+              </Col>
+              <Col>
+                <p>Your have {this.state.userTokens} CBR</p>
+              </Col>
+            </Row>
+          </div>
+          <div class="container" >
+            <Form onSubmit={this.handleSubmit}>
+              <Form.Row className="align-items-center">
+                <Col xs="auto" >
+                  <Form.Control placeholder="No of Tokens" onChange={this.handleChange} type="number" />
+                </Col>
+
+                <Col>
+                  <Button variant="primary" type="submit">
+                    Buy
+                  </Button>
+                </Col>
+              </Form.Row>
+            </Form>
+          </div>
+        </Jumbotron>
+        <div class="container" >
+          <ProgressBar xs={2} now={this.state.tokensSold * 100 / 750000} />
+          <br></br>
+          <h6>{this.state.tokensSold}/750000 Tokens Sold</h6>
+        </div>
+
+
+        {/* <form onSubmit={this.handleSubmit}>
           <input type="number" value={this.state.noOfTokens} onChange={this.handleChange} />
           <input type="submit" value="Buy" />
-        </form>
+        </form> */}
+
       </div>
     );
   }
